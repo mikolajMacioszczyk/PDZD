f69ddb2d2b28e8dcffa244cf459738fbced3708f
Mikołaj <macioszczyk.mikolaj@gmail.com>
2024-04-21 10:27:04 +0200

    print warning when stock data not found

--- stock_data_conversion/convert_json_csv.py
+++ stock_data_conversion/convert_json_csv.py
@@ -1,32 +1,33 @@
 import json
 import csv
 
-input_file = "./data.json"
-output_file = "./data.csv"
+input_file = "./stockdata.json"
+output_file = "./stockdata.csv"
 
-# Wczytaj dane z pliku JSON
 with open(input_file) as f:
     dane_json = json.load(f)
 
-# Otwórz plik CSV do zapisu
 with open(output_file, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
 
     general_header_names = ['symbol', 'name', 'sector', 'industry']
     stock_header_names = ['date', 'open', 'high', 'low', 'close', 'adjClose', 'volume', 'unadjustedVolume', 'change', 'changePercent', 'vwap', 'label', 'changeOverTime']
+    stock_data_column = 'stockData'
     all_header_names = general_header_names + stock_header_names
     writer.writerow(all_header_names)
 
-    # Przetwarzanie danych JSON i zapis do pliku CSV
+    # for every element in json table
     for row in dane_json:
-        # Sprawdź czy wiersz zawiera zagnieżdżoną tablicę
-        if 'stockData' in row:
-            # Dla każdego elementu zagnieżdżonej tablicy
-            for element in row['stockData']:
+        print("#", end='')
+        if stock_data_column in row:
+            # for every element of inner table
+            for element in row[stock_data_column]:
                 new_row = [row[header] for header in general_header_names]
                 new_row.extend(element.values())
                 writer.writerow(new_row)
         else:
-            raise Exception("stockData not found") 
+            print('warning: ' + stock_data_column + 'column not exists in json object: ')
+            print(row)
+            print('skipping')
 
 print("Conversion completed")
